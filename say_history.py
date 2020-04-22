00eb11af4571e25774651856068870087dd660b2
alex <alekseytroe@gmail.com>
2020-04-22 15:55:47 +0300

    say_hi

---
+++ say_history.py
@@ -0,0 +1,13 @@
+# 1. on CheckiO your solution should be a function
+# 2. the function should return the right answer, not print it.
+
+
+def say_hi(name: str, age: int) -> str:
+    """
+        Hi!
+    """
+    return f"Hi. My name is {name} and I'm {age} years old"
+
+
+print(say_hi("Alex", 32))
+
